24a08f56293ea34404133d9120f135286f2eeaeb
sumila-shakya <sumilashakya14@gmail.com>
2026-03-06 22:25:22 +0545

    add company profiles schema

--- server/src/models/mongodb.models.ts
+++ server/src/models/mongodb.models.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+//interface definition
 interface IJobSeeker {
     jobSeekerId: number,
     bio?: string,
@@ -17,6 +18,35 @@ interface IJobSeeker {
     isHidden: boolean
 }
 
+interface ICompany {
+    companyId: number,
+    aboutUs?: string,
+    specialties?: string[],
+    hqLocation?: { country: string, city: string },
+    contactNo?: string,
+    companyWebsiteURL?: string,  
+    isHidden: boolean
+}
+
+interface IJob {
+    jobId: number,
+    description?: string,
+    requirement: string[],
+    position: 'junior' | 'senior';
+    employmentType: 'full' | 'part' | 'intern' | 'contract' | 'freelance';
+    workType: 'remote' | 'on-site' | 'hybrid';
+    education?: {
+        level: 'Bachelor'|'Master'|'PhD'|'+2/A-levels'|'SEE'|'others',
+        field: string,
+    },
+    location: { country: string, city: string},
+    salary?: { min: number, max: number, currency: string},
+    experience?: { min: number, max: number},
+    category: string
+}
+
+
+//schema definition
 const jobSeekerSchema = new mongoose.Schema<IJobSeeker>({
     jobSeekerId: {type: Number, required: true, unique: true},
     bio: {type: String},
@@ -41,6 +71,50 @@ const jobSeekerSchema = new mongoose.Schema<IJobSeeker>({
     isHidden: {type: Boolean, default: false}
 },{timestamps: true})
 
-jobSeekerSchema.index({jobSeekerId: 1})
+const companySchema = new mongoose.Schema<ICompany>({
+    companyId: {type: Number, required: true, unique: true},
+    aboutUs: {type: String},
+    specialties: { type: [String]},
+    hqLocation: {
+        country: {type: String},
+        city: { type: String}
+    },
+    contactNo: {type: String, unique: true, sparse: true},
+    companyWebsiteURL: {type: String},
+    isHidden: {type: Boolean, default: false} 
+},{timestamps: true})
 
+const JobSchema = new mongoose.Schema<IJob>({
+    jobId: {type: Number, required: true, unique: true},
+    description: {type: String},
+    requirement: { type: [String]},
+    position: {type: String, enum:['junior' , 'senior']},
+    employmentType: {type: String, enum:['full' , 'part' , 'intern' , 'contract' , 'freelance']},
+    workType: {type: String, enum:['remote' , 'on-site' , 'hybrid']},
+    education: [{
+        level: {type: String, enum:['Bachelor','Master','PhD','+2/A-levels','SEE','others']},
+        field: {type: String}
+    }],
+    location: {
+        country: {type: String},
+        city: { type: String}
+    },
+    salary: {
+        min: {type: Number},
+        max: { type: Number},
+        currency: {type: String}
+    },
+    experience: {
+        min: {type: Number},
+        max: { type: Number},
+    },
+    category: {type: String}
+})
+
+//indexing
+
+
+
+//models
 export const JobSeekersProfile = mongoose.model<IJobSeeker>('JobSeekersProfile',jobSeekerSchema)
+export const CompanyProfile = mongoose.model<ICompany>('CompanyProfile',companySchema)
