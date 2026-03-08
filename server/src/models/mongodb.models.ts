7072bcbb86fed38b5974e4407348d87b6687a067
sumila-shakya <sumilashakya14@gmail.com>
2026-03-08 21:54:02 +0545

    add job detail schema

--- server/src/models/mongodb.models.ts
+++ server/src/models/mongodb.models.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { POSITION, EMPLOYMENT_TYPE, WORKTYPE, EDUCATION_LEVEL } from "../utils/constants";
 
 //interface definition
 interface IJobSeeker {
@@ -6,7 +7,7 @@ interface IJobSeeker {
     bio?: string,
     skills?: string[],
     education?: {
-        level: 'Bachelor'|'Master'|'PhD'|'+2/A-levels'|'SEE'|'others',
+        level: typeof EDUCATION_LEVEL[number],
         field: string,
         institution: string,
         year: number
@@ -32,11 +33,11 @@ interface IJob {
     jobId: number,
     description?: string,
     requirement: string[],
-    position: 'junior' | 'senior';
-    employmentType: 'full' | 'part' | 'intern' | 'contract' | 'freelance';
-    workType: 'remote' | 'on-site' | 'hybrid';
+    position: typeof POSITION[number];
+    employmentType: typeof EMPLOYMENT_TYPE[number];
+    workType: typeof WORKTYPE[number];
     education?: {
-        level: 'Bachelor'|'Master'|'PhD'|'+2/A-levels'|'SEE'|'others',
+        level: typeof EDUCATION_LEVEL[number],
         field: string,
     },
     location: { country: string, city: string},
@@ -52,7 +53,7 @@ const jobSeekerSchema = new mongoose.Schema<IJobSeeker>({
     bio: {type: String},
     skills: { type: [String]},
     education: [{
-        level: {type: String, enum:['Bachelor','Master','PhD','+2/A-levels','SEE','others']},
+        level: {type: String, enum:EDUCATION_LEVEL},
         field: {type: String},
         institution: {type: String},
         year: {type: Number} 
@@ -88,13 +89,13 @@ const JobSchema = new mongoose.Schema<IJob>({
     jobId: {type: Number, required: true, unique: true},
     description: {type: String},
     requirement: { type: [String]},
-    position: {type: String, enum:['junior' , 'senior']},
-    employmentType: {type: String, enum:['full' , 'part' , 'intern' , 'contract' , 'freelance']},
-    workType: {type: String, enum:['remote' , 'on-site' , 'hybrid']},
-    education: [{
-        level: {type: String, enum:['Bachelor','Master','PhD','+2/A-levels','SEE','others']},
+    position: {type: String, enum:POSITION},
+    employmentType: {type: String, enum:EMPLOYMENT_TYPE},
+    workType: {type: String, enum:WORKTYPE},
+    education: {
+        level: {type: String, enum:EDUCATION_LEVEL},
         field: {type: String}
-    }],
+    },
     location: {
         country: {type: String},
         city: { type: String}
@@ -112,9 +113,18 @@ const JobSchema = new mongoose.Schema<IJob>({
 })
 
 //indexing
-
-
+JobSchema.index({'category':1, 'location.city':1, 'position':1})
+JobSchema.index({'location.city':1, 'employmentType':1, 'workType':1})
+JobSchema.index(
+    {'salary.min':1, 'salary.max':1},
+    {partialFilterExpression: {'salary':{$exists:true}}}
+)
+JobSchema.index(
+    {'experience.min':1, 'experience.max':1},
+    {partialFilterExpression: {'experience':{$exists:true}}}
+)
 
 //models
 export const JobSeekersProfile = mongoose.model<IJobSeeker>('JobSeekersProfile',jobSeekerSchema)
 export const CompanyProfile = mongoose.model<ICompany>('CompanyProfile',companySchema)
+export const JobDetail = mongoose.model<IJob>('JobDetail',JobSchema)
---
+++ server/src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const POSITION = ['junior' , 'senior'] as const;
+export const EMPLOYMENT_TYPE = ['full' , 'part' , 'intern' , 'contract' , 'freelance'] as const;
+export const WORKTYPE = ['remote' , 'on-site' , 'hybrid'] as const;
+export const EDUCATION_LEVEL = ['Bachelor','Master','PhD','+2/A-levels','SEE','others'] as const;
